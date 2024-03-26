f88c439ee95fefd915f1cd1c10926f9c9f81501a
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-03-26 14:39:02 -0300

    .

--- count/src/App.jsx
+++ count/src/App.jsx
@@ -1,11 +1,23 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+
+import newyear from './assets/newyear.jpg';
 
 import './App.css'
 
 function App() {
 
   return (
-    <div className='app'>
-    <h1>count down</h1>
+    <div className='app' style={{backgroundImage: `url(${newyear})`}}>
+    <div className="container">
+   <Title title= "Contagem regressiva 2025"/>
+      <div className="countdown-container">
+        <Counter title="Dias" number={2}/>
+        <Counter title="Horas" number={2}/>
+        <Counter title="Minutos" number={2}/>
+        <Counter title="Segundos" number={2}/>
+      </div>
+    </div>
     </div>
   )
 }
