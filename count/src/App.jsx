b333c6e882bdaefb94b98e1b14dd441f5699dfbd
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-03-27 12:02:20 -0300

    .

--- count/src/App.jsx
+++ count/src/App.jsx
@@ -3,19 +3,22 @@ import Counter from './components/Counter'
 
 import newyear from './assets/newyear.jpg';
 
+import useCountDown from './hooks/useCountDown';
+
 import './App.css'
 
 function App() {
+  const [day, hour, minute, second] = useCountDown("jan 1, 2025 00:00")
 
   return (
     <div className='app' style={{backgroundImage: `url(${newyear})`}}>
     <div className="container">
    <Title title= "Contagem regressiva 2025"/>
       <div className="countdown-container">
-        <Counter title="Dias" number={2}/>
-        <Counter title="Horas" number={2}/>
-        <Counter title="Minutos" number={2}/>
-        <Counter title="Segundos" number={2}/>
+        <Counter title="Dias" number={day}/>
+        <Counter title="Horas" number={hour}/>
+        <Counter title="Minutos" number={minute}/>
+        <Counter title="Segundos" number={second}/>
       </div>
     </div>
     </div>
