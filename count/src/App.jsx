94273ca27ef7668875c01b13a160b474d024d695
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-03-29 19:27:27 -0300

    .

--- count/src/App.jsx
+++ count/src/App.jsx
@@ -1,27 +1,19 @@
-import Title from './components/Title'
-import Counter from './components/Counter'
+import { Outlet } from 'react-router-dom';
 
 import newyear from './assets/newyear.jpg';
 
-import useCountDown from './hooks/useCountDown';
+
 
 import './App.css'
 
 function App() {
-  const [day, hour, minute, second] = useCountDown("jan 1, 2025 00:00")
-
+ 
   return (
     <div className='app' style={{backgroundImage: `url(${newyear})`}}>
-    <div className="container">
-   <Title title= "Contagem regressiva 2025"/>
-      <div className="countdown-container">
-        <Counter title="Dias" number={day}/>
-        <Counter title="Horas" number={hour}/>
-        <Counter title="Minutos" number={minute}/>
-        <Counter title="Segundos" number={second}/>
-      </div>
+      <div className='container'>
+      <Outlet/>
     </div>
-    </div>
+   </div>
   )
 }
 
---
+++ count/src/routes/Home.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+
+import "./home.css"
+
+const Home = () => {
+    const [title, setTitle] = useState()
+    const [date, setDate] = useState()
+    const [image, setImage] = useState()
+    const [color, setColor] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const eventObject = {
+            title,
+            date,
+            image,
+            color
+        }
+        console.log(eventObject);
+    }
+
+  return (
+    <div className='home'>
+        <h2>Monte a sua contagem regressiva</h2>
+        <form className='countdown-form' onSubmit={handleSubmit}>
+            <label>
+                <span>Titulo:</span>
+                <input 
+                type="text"
+                name='title' 
+                placeholder='Digite o titulo do evento'
+                onChange={(e) => setTitle(e.target.value)}
+                required/>
+            </label>
+
+            <label>
+                <span>Data do evento:</span>
+                <input 
+                type="date"
+                name='date'
+                onChange={(e) => setDate(e.target.value)} 
+                required/>
+            </label>
+
+            <label>
+                <span>Imagem:</span>
+                <input 
+                type="text"
+                name='image' 
+                placeholder='Insira a url da imagem'
+                onChange={(e) => setImage(e.target.value)}/>
+            </label>
+
+            <label>
+                <span>Cor do tema:</span>
+                <input 
+                type="color"
+                name='color'
+                onChange={(e) => setColor(e.target.value)} />
+
+                <input 
+                type="submit" 
+                value="Enviar"/>
+            </label>
+        </form>
+    </div>
+  )
+}
+
+export default Home
