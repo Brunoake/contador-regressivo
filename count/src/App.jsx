269d8d82a085c4c7881e51ad7a3c626f1189ece1
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-04-01 14:58:41 -0300

    finalizado

--- count/src/App.jsx
+++ count/src/App.jsx
@@ -1,5 +1,9 @@
 import { Outlet } from 'react-router-dom';
 
+import { useContext } from 'react';
+
+import { CountdownContext } from './context/CountdownContext';
+
 import newyear from './assets/newyear.jpg';
 
 
@@ -7,9 +11,20 @@ import newyear from './assets/newyear.jpg';
 import './App.css'
 
 function App() {
+
+  const {event} = useContext(CountdownContext);
+
+  let eventImage = null;
+ 
+  if(event) eventImage = event.image;
  
   return (
-    <div className='app' style={{backgroundImage: `url(${newyear})`}}>
+    <div className='app' 
+    style={
+      eventImage
+      ? {backgroundImage: `url(${eventImage})`}
+      : {backgroundImage: `url(${newyear})`}
+    }>
       <div className='container'>
       <Outlet/>
     </div>
--- count/src/routes/Countdown.jsx
+++ count/src/routes/Countdown.jsx
@@ -1,20 +1,37 @@
 import Title from '../components/Title'
 import Counter from '../components/Counter'
 
+import { useContext } from 'react';
+
 import useCountDown from '../hooks/useCountDown';
 
+import { CountdownContext } from '../context/CountdownContext';
+
+import { Navigate } from 'react-router-dom';
+
 const Countdown = () => {
-    const [day, hour, minute, second] = useCountDown("jan 1, 2025 00:00")
+
+  const {event} = useContext(CountdownContext);
+
+  if(!event) return <Navigate to="/" />;
+
+  console.log(event)
+
+  const eventTitle = event.title;
+
+  const eventColor = event.color;
+
+    const [day, hour, minute, second] = useCountDown(event.date);
 
 
   return (
     <>
-    <Title title= "Contagem regressiva 2025"/>
+    <Title title= {eventTitle} eventColor={eventColor}/>
     <div className='countdown-container'>
-         <Counter title="Dias" number={day}/>
-         <Counter title="Horas" number={hour}/>
-         <Counter title="Minutos" number={minute}/>
-         <Counter title="Segundos" number={second}/>
+         <Counter title="Dias" number={day} eventColor={eventColor}/>
+         <Counter title="Horas" number={hour} eventColor={eventColor}/>
+         <Counter title="Minutos" number={minute} eventColor={eventColor}/>
+         <Counter title="Segundos" number={second} eventColor={eventColor}/>
          </div>
        </>
   )
